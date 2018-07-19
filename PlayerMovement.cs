082f25d44d1bb2693b6d4502883722105d7b0a3e
Zofy <32739550+zofy97@users.noreply.github.com>
2018-07-19 15:55:52 +0200

    Add files via upload

---
+++ PlayerMovement.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    // This is a reference to the Rigidbody component called "rb"
+    public Rigidbody rb;
+	
+	// We marked this as "FixedUpdate" because we
+    // are using it to mess with physics.
+	void FixedUpdate ()
+    {
+        rb.AddForce(0, 0, 2000 * Time.deltaTime);
+	}
+}
