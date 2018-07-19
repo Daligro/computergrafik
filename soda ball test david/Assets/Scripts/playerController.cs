1eb00e9155525031fe2d7edcafb718735ecaaf2b
David Linke <david.linke98@gmail.com>
2018-07-19 17:53:50 +0200

    test

---
+++ soda ball test david/Assets/Scripts/playerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+
+    public float speed;
+    public float jumpHeight;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        float jump = 0;
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            jump = 1 * jumpHeight;
+        }
+
+        Vector3 movement = new Vector3(moveHorizontal * speed, jump, moveVertical * speed);
+
+        rb.AddForce(movement);
+    }
+
+
+ }
