7db1c0f23b20806aa5093a4d4c50e9dd534764aa
David Linke <david.linke98@gmail.com>
2018-07-20 17:45:47 +0200

    Kamera mit Maus steuerbar, Ballsteuerung abhängig von Kamera. Probleme: Invertierung der Y-Achse bei 180 grad drehung

--- Assets/Scripts/cameraController.cs
+++ Assets/Scripts/cameraController.cs
@@ -5,16 +5,33 @@ using UnityEngine;
 public class cameraController : MonoBehaviour {
 
     public GameObject player;
+    public bool rotateAroundPlayer = true;
+    public float smoothFactor = 1;
+    public float RotationSpeed = 1.5f;
 
     private Vector3 offset;
 
 	// Use this for initialization
 	void Start () {
         offset = transform.position - player.transform.position;
+        //offset = transform.position - player.transform.position;
 	}
 	
 	// Update is called once per frame
 	void LateUpdate () {
-        transform.position = player.transform.position + offset;
+
+        if(rotateAroundPlayer)
+        {
+            Quaternion camTurnAngle1 = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * RotationSpeed, Vector3.up);
+            Quaternion camTurnAngle2 = Quaternion.AngleAxis(Input.GetAxis("Mouse Y") * RotationSpeed, Vector3.left);
+            offset = camTurnAngle1 * camTurnAngle2 * offset;
+        }
+
+        Vector3 newPos = player.transform.position + offset;
+
+        transform.position = Vector3.Slerp(transform.position, newPos, smoothFactor);
+
+        transform.LookAt(player.transform);
+
 	}
 }
--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class playerController : MonoBehaviour {
 
+    [Range(0.01f,10.0f)]
     public float speed;
+    [Range(0.1f,1000.0f)]
     public float jumpHeight;
+    [Range(0.1f,10.0f)]
+    public float friction;
 
+    public GameObject mainCamera;
     public AudioClip jumpSound;
     public AudioSource MusicSource;
 
@@ -17,33 +22,52 @@ public class playerController : MonoBehaviour {
     {
         rb = GetComponent<Rigidbody>();
         MusicSource.clip = jumpSound;
+        rb.angularDrag = friction;
     }
     void FixedUpdate()
     {
+        Vector3 fromCameraToMe = transform.position - mainCamera.transform.position;
+        fromCameraToMe.y = 0;
+        fromCameraToMe.Normalize();
+        //Vector2 movementVector = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        //float force = speed * movementVector.magnitude;
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
+
         float jump = 0;
 
-        if(Input.GetButton("Jump") && canJump)
+        if(canJump)
+        {
+            rb.angularDrag = friction;
+        }
+        else
+        {
+            rb.angularDrag = 0.1f;
+        }
+
+        if (Input.GetButton("Jump") && canJump)
         {
             jump = 1 * jumpHeight;
             MusicSource.Play();
         }
-
-        Vector3 movement = new Vector3(moveHorizontal * speed, jump, moveVertical * speed);
+        Vector3 movement = (fromCameraToMe * moveVertical + mainCamera.transform.right * moveHorizontal)*speed;
+        movement.y += jump;
+        //Vector3 direction = movementVector.y *  + jump + movementVector.x * maincam.;
+        //Vector3 movement = new Vector3(moveHorizontal * speed, jump, moveVertical * speed);
 
         rb.AddForce(movement);
     }
 
     void OnCollisionEnter(Collision other)
     {
-        print("test");
+        print("jump");
         if (other.gameObject.tag.Equals("Floor")) 
             canJump = true;
     }
 
     void OnCollisionExit(Collision other)
     {
+        print("land");
         if (other.gameObject.tag.Equals("Floor"))
             canJump = false;
     }
