7416f294467a482491d36842277a02294ad81133
s0559289 <s0559289@htw-berlin.de>
2018-07-22 01:16:10 +0200

    added thirdPersonCamera - diesmal wirklich

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour {
 
     [Range(0.01f, 30.0f)]
     public float speed;
