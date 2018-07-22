74e726eeef248dcd85aa8da8e6fa8a0838bfecf1
s0559289 <s0559289@htw-berlin.de>
2018-07-22 23:54:08 +0200

    added coin functionality (without GUIText)

---
+++ Assets/Scripts/PickCoin.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickCoin : MonoBehaviour {
+
+    public AudioClip coinSound;
+    //public GUIText coinText;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        AudioSource.PlayClipAtPoint(coinSound, transform.position);
+        //int score = int.Parse (coinText.text) + 1;
+        //coinText.text = score.ToString();
+        Destroy(gameObject);
+    }
+}
