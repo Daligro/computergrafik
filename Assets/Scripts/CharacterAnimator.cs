1431dafbdcd0ff985cfcf305c68633b7fd32e67d
s0559289 <s0559289@htw-berlin.de>
2018-07-23 17:15:22 +0200

    added enemy skin, changed map, enemies still buggy

---
+++ Assets/Scripts/CharacterAnimator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CharacterAnimator : MonoBehaviour {
+
+    const float locomationAnimationSmoothTime = .1f;
+
+    NavMeshAgent agent;
+    protected Animator animator;
+    public AnimatorOverrideController overrideController;
+
+	protected virtual void Start () {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponentInChildren<Animator>();
+
+        if (overrideController == null)
+        {
+            overrideController = new AnimatorOverrideController(animator.runtimeAnimatorController);
+        }
+        animator.runtimeAnimatorController = overrideController;
+
+	}
+    protected virtual void Update()
+    {
+        float speedPercent = agent.velocity.magnitude / agent.speed;
+        animator.SetFloat("speedPercent", speedPercent, locomationAnimationSmoothTime, Time.deltaTime);
+    }
+}
