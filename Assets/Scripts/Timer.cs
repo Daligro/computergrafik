964aea09a7df7b8725a7827aeb9f5acca9513bbf
s0559289 <s0559289@htw-berlin.de>
2018-07-24 12:07:17 +0200

    map changes, added description screen

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    public GUIText timeText;
+
+	// Use this for initialization
+	void Start ()
+    {
+        InvokeRepeating("IncreaseTime", 1, 1);
+	}
+
+    // Update is called once per frame
+    void IncreaseTime()
+    {
+        int currentTime = int.Parse(timeText.text) + 1;
+        timeText.text = currentTime.ToString();
+
+    }    
+}
